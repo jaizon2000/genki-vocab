bf0efa984271ab65dc48e89f49f112db1f534e13
Jehdi <46204336+jaizon2000@users.noreply.github.com>
2020-04-05 17:26:24 -0600

    adding jap content

--- dataLesson.js
+++ dataLesson.js
@@ -19,8 +19,7 @@ var l11 = [{
         },
         def: "(hand)",
         examples: ["手紙 (てがみ) letter", "手 (て) hand", "上手な (じょうずな) good at", "歌手 (かしゅ) singer", "手話 (しゅわ) sign language"]
-    },
-    {
+    }, {
         kanji: "紙",
         reading: {
             onyomi: "し",
@@ -28,19 +27,112 @@ var l11 = [{
         },
         def: "(paper)",
         examples: ["手紙 (てがみ) letter", "紙 (かみ) paper", "表紙 (ひょうし) front cover", "和紙 (わし) Japanese paper"]
-    },
-    {
+    }, {
         kanji: "好",
         reading: {
             onyomi: "こう",
             kunyomi: "す　この"
         },
         def: "(favorite; to like)",
-        examples: ["好きな (すきな) to like", "好意 (こうい) good will", "好物 (こうぶつ) favorite food","大好き (だいすき) to love","好み (このみ) liking; taste"]
+        examples: ["好きな (すきな) to like", "好意 (こうい) good will", "好物 (こうぶつ) favorite food", "大好き (だいすき) to love", "好み (このみ) liking; taste"]
 
     },
     {
-        kanji: "",
+        kanji: "近",
+        reading: {
+            onyomi: "きん",
+            kunyomi: "ちか"
+        },
+        def: "(near)",
+        examples: ["近く (ちかく) near; nearby", "最近 (さいきん) recently", "中近東 (ちゅうきんとう) the Middle and Near East", "近所 (きんじょ) neigbourhood"]
+    }, {
+        kanji: "明",
+        reading: {
+            onyomi: "めい",
+            kunyomi: "あか"
+        },
+        def: "(bright)",
+        examples: ["明るい (あかるい) cheerful; bright", "明日 (あした) tomorrow","発明 (はつめい) invention", "説明 (せつめい) explanation", "文明 (ぶんめい) civilization"]
+    }, {
+        kanji: "病",
+        reading: {
+            onyomi: "びょう",
+        },
+        def: "(ill; sick)",
+        examples: ["病院 (びょういん) hospital", "重病 (じゅうびょう) serious illness", "急病 (きゅうびょう) sudden illness", "病気 (びょうき) illness"]
+    }, {
+        kanji: "院",
+        reading: {
+            onyomi: "いん",
+        },
+        def: "(institution)",
+        examples: ["病院 (びょういん) hospital", "大学院 (だいがくいん) graduate school", "美容院 (びよういん) beauty parlor"]
+    }, {
+        kanji: "映",
+        reading: {
+            onyomi: "えい",
+            kunyomi: "うつ"
+        },
+        def: "(to reflect)",
+        examples: ["映画 (えいが) movie", "映画館 (えいがかん) movie theater", "映る (うつる) to be reflected"]
+    }, {
+        kanji: "画",
+        reading: {
+            onyomi: "",
+            kunyomi: ""
+        },
+        def: "()",
+        examples: [" () ", " () "]
+    }, {
+        kanji: "歌",
+        reading: {
+            onyomi: "",
+            kunyomi: ""
+        },
+        def: "()",
+        examples: [" () ", " () "]
+    }, {
+        kanji: "市",
+        reading: {
+            onyomi: "",
+            kunyomi: ""
+        },
+        def: "()",
+        examples: [" () ", " () "]
+    }, {
+        kanji: "所",
+        reading: {
+            onyomi: "",
+            kunyomi: ""
+        },
+        def: "()",
+        examples: [" () ", " () "]
+    }, {
+        kanji: "勉",
+        reading: {
+            onyomi: "",
+            kunyomi: ""
+        },
+        def: "()",
+        examples: [" () ", " () "]
+    }, {
+        kanji: "強",
+        reading: {
+            onyomi: "",
+            kunyomi: ""
+        },
+        def: "()",
+        examples: [" () ", " () "]
+    }, {
+        kanji: "有",
+        reading: {
+            onyomi: "",
+            kunyomi: ""
+        },
+        def: "()",
+        examples: [" () ", " () "]
+    }, {
+        kanji: "旅	",
         reading: {
             onyomi: "",
             kunyomi: ""
